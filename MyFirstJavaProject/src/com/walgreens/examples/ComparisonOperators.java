e370cea28b86eed5ec722407b253b001d022b9e2
Sanmam18 <sanmam18@gmail.com>
2015-10-03 14:20:07 -0700

    Initial commit for Comparison Operators
    Initial commit for Comparison Operators

---
+++ MyFirstJavaProject/src/com/walgreens/examples/ComparisonOperators.java
@@ -0,0 +1,29 @@
+package com.walgreens.examples;
+
+public class ComparisonOperators {
+
+	public static void main(String[] args) {
+		compareNumbers();
+	}
+	
+	/**
+	 * 
+	 */
+	public static void compareNumbers() {
+		
+		int no1 = 10;
+		int no2 = 20;
+		
+		if(no1 > no2) {
+			System.out.println("no1 is greater than no2");
+		}
+		
+		if (no1 < no2) {
+			System.out.println("no2 is greater than no1");
+		}
+		
+		
+		
+	}
+
+}
